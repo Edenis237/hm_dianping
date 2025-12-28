3c446c78e8fa9c5902d124c58dc9f27b61b6468e
dingjiaao <1157607108@qq.com>
2025-12-28 17:33:47 +0800

    乐观锁解决一人多单超卖问题

--- src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
@@ -88,7 +88,8 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
         // 扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId).gt("stock",0)
+                .update();
 
         if(!success){
             return Result.fail("库存不足");
